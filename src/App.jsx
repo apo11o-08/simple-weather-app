075603efb3f3526078cbceae32fcb6ccc7d408db
Seth Diedricks <sethdiedricks@student.unsw.edu.au>
2026-03-09 20:58:27 +1100

    added json file instead of hardcoded city-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,25 @@
 import { useState, useEffect } from 'react'
 import {fetchWeatherApi} from "openmeteo"
 import './App.css'
-
-const SYD_LAT = 2;
-const SYD_LON = 30;
+import citiesData from './city_data.json';
 const DEC_PLACES = 2;
 
-const coords = {
-  "SYDNEY":
-    {
-      LONG: 151.2073,
-      LAT: -33.8708,
-    },
-  "MELBOURNE":
-    {
-      LONG: 144.9623,
-      LAT: -37.8124,
-    },
-  "ADELAIDE":
-    {
-      LONG: 138.6007,
-      LAT: -34.9285,
-    },
-  "CAIRNS":
-    {
-      LONG: 145.7710,
-      LAT: -16.9203
-    },
-}
-const cities = ["SYDNEY", "MELBOURNE", "ADELAIDE", "CAIRNS"];
+
 
 function App() {
+
+
   const [temperature, setTemp] = useState(0);
-  const [currentCity, setCurrentCity] = useState(cities[0]);
+  const [currentCity, setCurrentCity] = useState(citiesData.city_arr[0].name);
   const [loading, setLoading] = useState(true);
+
 
   const fetchWeather = async () => {
     setLoading(true);
+    const city = citiesData.city_arr.find(c => c.name === currentCity);
     const params = {
-      latitude: coords[currentCity].LAT,
-      longitude: coords[currentCity].LONG,
+      latitude: city.LAT,
+      longitude: city.LONG,
       hourly: "temperature_2m",
     };
 
@@ -66,15 +46,15 @@ function App() {
       <h1 className='temp'>{loading ? "Loading..." : temperature.toFixed(DEC_PLACES) + "°C"}</h1>
 
       <div className='city-buttons'>
-        {cities.map((city) => (
+        {citiesData.city_arr.map((city) => (
           <button
-            key={city}
+            key={city.name}
             onClick={() => {
-              setCurrentCity(city);  // update state
+              setCurrentCity(city.name);  // update state
             }}
-            className={city == currentCity ? 'active' : ""}
+            className={city.name == currentCity ? 'active' : ""}
           >
-            {city}
+            {city.name}
           </button>
         ))}
       </div>
